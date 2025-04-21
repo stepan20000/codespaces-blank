218572e24952df89f1d6bd4fcd057e5d4aaaa22e
Stepan Yefimov <stepan.yefimov@gmail.com>
2025-04-21 12:49:15 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import { genkit } from 'genkit';
+import { googleAI, gemini20Flash } from '@genkit-ai/googleai';
+
+const ai = genkit({ plugins: [googleAI()] });
+
+const { text } = await ai.generate({
+    model: gemini20Flash,
+    prompt: 'Why is Firebase awesome?'
+});
